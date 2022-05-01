0ae8c2ec3491d0841a1b1fc15aeaa012ca23755c
Chase <70964202+Chase1803@users.noreply.github.com>
2022-05-01 10:18:04 +0530

    Update and rename Main.cpp to decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -1,9 +1,9 @@
-byte* buffer = (byte*)VirtualAlloc(nullptr, nt.OptionalHeader.SizeOfCode, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+                byte* buffer = (byte*)VirtualAlloc(nullptr, nt.OptionalHeader.SizeOfCode, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
 		if (!buffer) {
 			return false;
 		}
 
-uintptr_t uworld_addr = PatternScanner((uint8_t*)buffer, nt.OptionalHeader.SizeOfCode, (char*)"\x48\x8D\x05\x00\x00\x00\x00\x45\x8B\xD3\x4E\x8B\x04\xD8\xB8\x25\x49\x92\x24\x41\xF7\xE3", (char*)"xxx????xxxxxxxxxxxxxxx");
+                uintptr_t uworld_addr = PatternScanner((uint8_t*)buffer, nt.OptionalHeader.SizeOfCode, (char*)"\x48\x8D\x05\x00\x00\x00\x00\x45\x8B\xD3\x4E\x8B\x04\xD8\xB8\x25\x49\x92\x24\x41\xF7\xE3", (char*)"xxx????xxxxxxxxxxxxxxx");
 		if (!uworld_addr) { 
                         return false; 
                 }
