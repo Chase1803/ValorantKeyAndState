24e3702673377d4815677a1b9d4c4249040fc11b
Chase <70964202+Chase1803@users.noreply.github.com>
2022-05-01 10:25:26 +0530

    Update decrypt.cpp

--- decrypt.cpp
+++ decrypt.cpp
@@ -2,9 +2,9 @@
 		if (!buffer) {
 			return false;
 		}
-
+//48 8D 05 ? ? ? ? 45 8B D3 4E 8B 04 D8 B8 25 49 92 24 41 F7 E3
                 uintptr_t uworld_addr = PatternScanner((uint8_t*)buffer, nt.OptionalHeader.SizeOfCode, (char*)"\x48\x8D\x05\x00\x00\x00\x00\x45\x8B\xD3\x4E\x8B\x04\xD8\xB8\x25\x49\x92\x24\x41\xF7\xE3", (char*)"xxx????xxxxxxxxxxxxxxx");
-		if (!uworld_addr) { 
+		if (!uworld_addr) {
                         return false; 
                 }
 
